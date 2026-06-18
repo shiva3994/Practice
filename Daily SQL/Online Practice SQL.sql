06fca659adb6974c30543d0279e3baba518c4200
SHIVA THAKUR <shiv39941@gmail.com>
2026-06-18 14:16:27 +0530

    Update Online Practice SQL.sql

--- Daily SQL/Online Practice SQL.sql
+++ Daily SQL/Online Practice SQL.sql
@@ -203,4 +203,9 @@ select
   SUM(Gender = 'F') AS female_count
 from patients;
 
+-- Show the total amount of male patients and the total amount of female patients in the patients table.
+-- Display the two results in the same row.
 
+select 
+		(select count(gender) from patients where gender = 'M') as male_count,
+        (select count(gender) from patients where gender = 'F') as female_count
