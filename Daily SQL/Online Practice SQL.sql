6e4152d0360bb3b7e528c38e6e73becb491a2146
SHIVA THAKUR <shiv39941@gmail.com>
2026-06-26 00:33:55 +0530

    Update Online Practice SQL.sql

--- Daily SQL/Online Practice SQL.sql
+++ Daily SQL/Online Practice SQL.sql
@@ -493,5 +493,23 @@ select
             AS 'gender_type'
 from patients;
 
+-- Show patient_id, first_name, last_name from patients whose does not have any records in the admissions table. 
+-- (Their patient_id does not exist in any admissions.patient_id rows.)
+
+select
+		p.patient_id,
+        first_name,
+        last_name
+from patients p
+where p.patient_id not in (select admissions.patient_id
+                           from admissions);
+
+
+
+
+
+
+
+
 
 
