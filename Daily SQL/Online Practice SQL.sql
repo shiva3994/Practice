48c3430ea0773d9afc4fde05e15adf7039565026
SHIVA THAKUR <shiv39941@gmail.com>
2026-06-26 00:43:46 +0530

    Update Online Practice SQL.sql

--- Daily SQL/Online Practice SQL.sql
+++ Daily SQL/Online Practice SQL.sql
@@ -504,7 +504,17 @@ from patients p
 where p.patient_id not in (select admissions.patient_id
                            from admissions);
 
+-- Show all of the patients grouped into weight groups.
+-- Show the total amount of patients in each weight group.
+-- Order the list by the weight group decending.
+-- For example, if they weight 100 to 109 they are placed in the 100 weight group, 110-119 = 110 weight group, etc.
 
+select
+		count(*) as patients_in_group,
+        floor(weight/10)*10 as weight_group
+from patients
+group by weight_group
+order by weight_group desc;
 
 
 
