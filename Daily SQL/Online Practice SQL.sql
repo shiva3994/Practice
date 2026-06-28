188b7d4d02f16a6cba5c9d7ffe1a24ad9a5e5a45
SHIVA THAKUR <shiv39941@gmail.com>
2026-06-29 01:18:06 +0530

    Update Online Practice SQL.sql

--- Daily SQL/Online Practice SQL.sql
+++ Daily SQL/Online Practice SQL.sql
@@ -535,7 +535,6 @@ SELECT
             then "Male"
             else "Female"
             End as gender_type
-            
 from patients;
 
 -- Show patient_id, first_name, last_name from patients whose does not have any records in the admissions table. 
@@ -625,3 +624,16 @@ FROM patients;
 -- Use CAST(variable_name AS FLOAT) function if you are dividing by two variables.
 
 -- ************************************************************************************************ --
+
+-- Show all of the patients grouped into weight groups.
+-- Show the total amount of patients in each weight group.
+-- Order the list by the weight group decending.
+-- For example, if they weight 100 to 109 they are placed in the 100 weight group, 110-119 = 110 weight group, etc.
+
+select
+        floor(weight/10)*10 as weight_group,
+        count(*) as patients_in_group
+from patients
+group by weight_group
+order by weight_group desc;
+
