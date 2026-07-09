9be5b685af0364928853a6eff09aefeb8f15d705
SHIVA THAKUR <shiv39941@gmail.com>
2026-07-10 00:47:02 +0530

    Update Online Practice SQL.sql

--- Daily SQL/Online Practice SQL.sql
+++ Daily SQL/Online Practice SQL.sql
@@ -904,10 +904,75 @@ JOIN invoice inv ON cu.id = inv.customer_id
 GROUP BY co.country_name
 HAVING AVG(inv.total_price) > (SELECT AVG(total_price) FROM invoice);
 
+-- Show first name and last name of patients who does not have allergies. (null)
 
+select
+		first_name,
+        last_name
+from patients
+where allergies is NULL;
 
+-- Show first name of patients that start with the letter 'C'
 
+select
+		first_name
+from patients
+where first_name like "C%";
 
+-- Show first name and last name of patients that weight within the range of 100 to 120 (inclusive)
 
+select
+		first_name,
+        last_name
+from patients
+where weight between 100 and 120;
 
+-- Update the patients table for the allergies column. If the patient's allergies is null then replace it with 'NKA'
 
+update patients
+set allergies = 'NKA'
+where allergies is null;
+
+-- Show first name and last name concatinated into one column to show their full name.
+
+select
+		concat(first_name," ",last_name) as full_name
+from patients;
+
+-- Show first name, last name, and the full province name of each patient.
+-- Example: 'Ontario' instead of 'ON'
+
+select
+		first_name,
+        last_name,
+        pn.province_name
+from patients p
+join province_names pn
+on p.province_id = pn.province_id;
+
+-- Show how many patients have a birth_date with 2010 as the birth year.
+
+select
+		count(birth_date) as total_patients
+from patients
+where year(birth_date) = 2010;
+
+-- Show the first_name, last_name, and height of the patient with the greatest height.
+
+select
+		first_name,
+        last_name,
+        height
+from patients
+where (select
+       		max(height)
+       from patients)
+order by height desc
+limit 1;
+
+-- Show all columns for patients who have one of the following patient_ids:
+-- 1,45,534,879,1000
+
+select *
+from patients
+where patient_id IN (1,45,534,879,1000)
