a9ab3fc05b9cbd3a75e5ec8f69272a799b1b877d
SHIVA THAKUR <shiv39941@gmail.com>
2026-07-12 15:24:01 +0530

    Update Online Practice SQL.sql

--- Daily SQL/Online Practice SQL.sql
+++ Daily SQL/Online Practice SQL.sql
@@ -1003,5 +1003,32 @@ select distinct (city) as unique_city
 from patients
 where province_id = 'NS';
 
+-- Write a query to find the first_name, last name and birth date of patients who has height greater than 160 and weight greater than 70
+
+select 
+		first_name,
+        last_name,
+        birth_date
+from patients
+where height > 160 and weight > 70;
+
+-- Write a query to find list of patients first_name, last_name, and allergies where allergies are not null and are from the city of 'Hamilton'
+
+select
+		first_name,
+        last_name,
+        allergies
+from patients
+where city = 'Hamilton' and allergies is not null;
+
+-- Show unique birth years from patients and order them by ascending.
+
+select distinct(Year(birth_date)) as birth_year
+from patients
+order by birth_date
+
+
+
+
 
 
