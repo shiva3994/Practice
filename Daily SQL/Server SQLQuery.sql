81b18b95a671d8e382bd1c64a8c38c96110be211
Shiva Thakur <63214655+shiva3994@users.noreply.github.com>
2026-08-01 18:36:14 +0530

    U

---
+++ Daily SQL/Server SQLQuery.sql
@@ -0,0 +1,11 @@
+CREATE TABLE persons(
+	id INT NOT NULL,
+	person_name VARCHAR(50) NOT NULL,
+	birth_date DATE,
+	phone VARCHAR(15) NOT NULL,
+	CONSTRAINT pk_persons PRIMARY KEY (id)
+	)
+
+SELECT * FROM persons
+
+Add new column called 
