92f166449a24bdbaedb355e938c86206ae1c198c
Shiva Thakur <63214655+shiva3994@users.noreply.github.com>
2026-08-05 13:55:43 +0530

    Create Gre_db.sql

---
+++ Daily SQL/Gre_db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employee(
+						employee_id SERIAL PRIMARY KEY,
+						name VARCHAR(100) NOT NULL,
+						position VARCHAR(100),
+						department VARCHAR(50),
+						hiring_date DATE,
+						salary NUMERIC(10,2)
+);
